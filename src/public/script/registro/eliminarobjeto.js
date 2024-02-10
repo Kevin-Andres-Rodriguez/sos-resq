2d47cc7d774fc56441a944b3094a3c53180cb7cd
JessicaGaby <132677074+JessicaGaby@users.noreply.github.com>
2024-02-10 00:05:21 -0500

    alert funcional

--- src/public/script/registro/eliminarobjeto.js
+++ src/public/script/registro/eliminarobjeto.js
@@ -1,15 +1,16 @@
-document.getElementById('boton1').onclick = function(event) {
-    // Prevenir la acción predeterminada del enlace
-    event.preventDefault();
+document.querySelectorAll('.botonEliminar').forEach(function(boton) {
+    boton.onclick = function(event) {
+        // Prevenir la acción predeterminada del enlace
+        event.preventDefault();
 
-    // Obtener el ID del registro familiar desde el botón
-    var id_objeto = this.getAttribute('data-id');
+        // Obtener el ID del registro familiar desde el botón
+        var id_objeto = this.getAttribute('data-id');
 
+        var confirmacion = confirm('¿Estás seguro que quieres eliminar el registro? Presiona "Aceptar" para eliminar o "Cancelar" para no hacerlo.');
 
-    var confirmacion = confirm('¿Estás seguro que quieres eliminar el registro? Presiona "Aceptar" para eliminar o "Cancelar" para no hacerlo.');
-
-    if (confirmacion) {
-        // Si el usuario hace clic en "Aceptar", redirigir a la URL de eliminación
-        window.location.href = '/objeto/eliminar/' + id_objeto;
-    }
-};
+        if (confirmacion) {
+            // Si el usuario hace clic en "Aceptar", redirigir a la URL de eliminación
+            window.location.href = '/objeto/eliminar/' + id_objeto;
+        }
+    };
+});
