716b18d196279759e2be670a37d5b70d254ecbd5
Kevin-Andres-Rodriguez <kaa.rodriguez@yavirac.edu.ec>
2024-02-09 22:32:58 -0500

    función del eliminar bien hecho

--- src/public/script/registro/mensaje_personalizado.js
+++ src/public/script/registro/mensaje_personalizado.js
@@ -1,16 +1,16 @@
-// alerta del eliminar
-document.getElementById('boton1').onclick = function(event) {
-    // Prevenir la acción predeterminada del enlace
-    event.preventDefault();
+document.querySelectorAll('.botonEliminar').forEach(function(boton) {
+    boton.onclick = function(event) {
+        // Prevenir la acción predeterminada del enlace
+        event.preventDefault();
 
-    // Obtener el ID del registro familiar desde el botón
-    var id_mensaje_personalizado = this.getAttribute('data-id');
+        // Obtener el ID del registro familiar desde el botón
+        var id_mensaje_personalizado = this.getAttribute('data-id');
 
+        var confirmacion = confirm('¿Estás seguro que quieres eliminar el registro? Presiona "Aceptar" para eliminar o "Cancelar" para no hacerlo.');
 
-    var confirmacion = confirm('¿Estás seguro que quieres eliminar el registro? Presiona "Aceptar" para eliminar o "Cancelar" para no hacerlo.');
-
-    if (confirmacion) {
-        // Si el usuario hace clic en "Aceptar", redirigir a la URL de eliminación
-        window.location.href = '/mensaje_personalizado/eliminar/' + id_mensaje_personalizado;
-    }
-};
+        if (confirmacion) {
+            // Si el usuario hace clic en "Aceptar", redirigir a la URL de eliminación
+            window.location.href = '/mensaje_personalizado/eliminar/' + id_mensaje_personalizado;
+        }
+    };
+});
